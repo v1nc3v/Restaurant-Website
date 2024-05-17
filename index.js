ac4450ef8e261e8104efeaf655056e62e13ea80d
Vincent <vton952@gmail.com>
2024-05-17 14:50:23 -0400

    may 17 (2)

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ let app = express();
 const path = require('path');
 
 const ejs = require('ejs');
-app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 
